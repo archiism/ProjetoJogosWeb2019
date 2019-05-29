d71db0c72c88af0965f14667a05af3b84ef3d43c
Luiz <Luiz@10.10.85.110>
2019-05-29 20:46:40 -0300

    luiz

--- Projeto/src/Dao/UsuarioDAO.java
+++ Projeto/src/Dao/UsuarioDAO.java
@@ -3,6 +3,7 @@ package Dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 
@@ -13,6 +14,8 @@ public class UsuarioDAO {
 	
 	private Connection con=null;
 	private String sql="";
+	PreparedStatement pst;
+	ResultSet rs;
 	
 	
 	public Date converte(String data) {    
@@ -43,6 +46,41 @@ public class UsuarioDAO {
 		return true;
 	}
 	
+	public UsuarioDTO Login(UsuarioDTO usuarioDTO) throws Exception
+	{
+		UsuarioDTO dto=null;
+		
+		try
+		{
+			if(!VerifiqueConexao())
+				return dto;
+			
+			sql="SELECT * FROM USUARIO WHERE LOGIN=? AND SENHA=?";
+			pst=con.prepareStatement(sql);
+			pst.setString(1, usuarioDTO.getLogin());
+			pst.setString(2, usuarioDTO.getSenha());
+			rs=pst.executeQuery();
+			
+			while(rs.next())
+			{
+				if(rs.getString("Login").equals(usuarioDTO.getLogin()) && rs.getString("Senha").equals(usuarioDTO.getSenha()))
+				{
+					dto=new UsuarioDTO();
+					dto.setLogin(rs.getString("Login"));
+					dto.setNome(rs.getString("Nome"));
+					dto.setLogin(rs.getString("Login"));
+					return dto;
+				}
+			}
+			
+		}catch(Exception e)
+		{
+			throw new Exception("Erro "+e.getMessage());
+		}
+		return dto;
+		
+	}
+	
 	public Boolean incluir(UsuarioDTO usuarioDTO) throws Exception
 	{
 		try 
@@ -51,7 +89,7 @@ public class UsuarioDAO {
 				return false;
 			
 			sql="INSERT INTO USUARIO (nome,senha,login,telefone,email,cpf,dataNascimento,nivelAcesso) VALUES (?,?,?,?,?,?,?,?);";
-			PreparedStatement pst=con.prepareStatement(sql);
+			pst=con.prepareStatement(sql);
 			pst.setString(1, usuarioDTO.getNome());
 			pst.setString(2, usuarioDTO.getSenha());
 			pst.setString(3, usuarioDTO.getLogin());
