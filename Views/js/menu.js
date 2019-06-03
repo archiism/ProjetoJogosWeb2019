de835a3c6d4e946132ab344e35e245c7de3e0da9
archiism <42035282+archiism@users.noreply.github.com>
2019-06-03 16:52:44 -0300

    commit

--- Views/js/menu.js
+++ Views/js/menu.js
@@ -1,17 +1,23 @@
 var modal =document.querySelector('#abrir2')
-
+var contador = -1;
 
 modal.addEventListener('click',function(){
     event.preventDefault();
     
-    var contador = 0;
-    if(contador % 2 === 0){         
+    contador++
+    
+    if(contador % 2 == 0){         
         window.location="#abrir";
-        contador++   
+        console.log('par')
+        modal.style.backgroundColor = 'red';
     }
     if(contador % 2 != 0){
         console.log('impar')
-        modal.Style.opacity = 0;
+        window.location='#fechar'
+        console.log(contador)
+        modal.style.backgroundColor = '#78cafa';
+        
     }
+    
 })  
 
