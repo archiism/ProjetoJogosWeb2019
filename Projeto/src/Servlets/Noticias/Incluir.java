07bc828cde7af4561f274f208ba89cf526206023
candidoluiz <luiz.fernando360@gmail.com>
2019-06-09 18:43:08 -0300

    luiz

--- Projeto/src/Servlets/Noticias/Incluir.java
+++ Projeto/src/Servlets/Noticias/Incluir.java
@@ -7,16 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Bus.NoticiaBus;
+import Dto.NoticiaDto;
+
 /**
  * Servlet implementation class Incluir
  */
 @WebServlet(name = "CadNoticia", urlPatterns = { "/CadNoticia" })
 public class Incluir extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private NoticiaDto noticiaDto = new NoticiaDto();
+    private NoticiaBus noticiaBus = new NoticiaBus();
+    
     public Incluir() {
         super();
         // TODO Auto-generated constructor stub
@@ -24,6 +26,24 @@ public class Incluir extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String titulo=request.getParameter("txtTitulo");
+		String assunto=request.getParameter("txtAssunto");
+		
+		noticiaDto.setAssunto(titulo);
+		noticiaDto.setTexto(assunto);
+		try
+		{
+			//if(noticiaBus.Incluir(noticiaDto))
+			//{
+				response.sendRedirect("./menu.jsp");
+			//}
+			
+		}catch(Exception e)
+		{
+			e.getMessage();
+		}
+		
+		
 		
 	}
 
---
+++ Projeto/src/Servlets/Noticias/Listar.java
@@ -0,0 +1,40 @@
+package Servlets.Noticias;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Listar
+ */
+@WebServlet(name = "ListarNoticias", urlPatterns = { "/ListarNoticias" })
+public class Listar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Listar() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try
+		{
+			
+		}catch(Exception e)
+		{
+			e.getMessage();
+		}
+	}
+
+	
+
+}
